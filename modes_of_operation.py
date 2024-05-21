ee6f286766f35d85caab87e9f05c48066c0ce881
Jaron Li <2002jaron@gmail.com>
2024-05-21 10:07:11 -0700

    making progress on ecb

--- modes_of_operation.py
+++ modes_of_operation.py
@@ -2,14 +2,27 @@ import os
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 
+def pad_fun():
+    ...
+    
+def read_file(f):
+    with open(f, "rb") as fil:
+        header = f.read(54)
+        data = f.read()
+    return header, data
+
 def generate_key():
     return get_random_bytes(16)
 
 def generate_iv():
     return get_random_bytes(16)
 
-def ebc_encrypt(file, key):
-    ...
+def ecb_encrypt(file, key):
+    cipher = AES.new(key,AES.MODE_ECB)
+    cipher_text = b""
+    header, data = read_file(file)
+    pad_data = pad_fun(data)
+
 def cbc_encrypt(file, key, iv):
     ...
 
