ae01e279d54f4d32d42e4e668c56cfa7076960b6
jaronli3 <2002jaron@gmail.com>
2024-05-21 19:56:19 -0700

    finished ecb encrypt and decrypt to check

--- modes_of_operation.py
+++ modes_of_operation.py
@@ -6,6 +6,9 @@ def pad_fun(data, block_size=16):
     len_padding = block_size - (len(data) % block_size)
     return data + bytes([len_padding] * len_padding)
 
+def unpad_fun(data):
+    return data[:-data[-1]]
+
 def read_file(f):
     with open(f, "rb") as fil:
         header = fil.read(54)
@@ -23,15 +26,23 @@ def generate_key():
 def generate_iv():
     return get_random_bytes(16)
 
-def ecb_encrypt(f, key):
+def ecb_encrypt(data, key):
     cipher = AES.new(key,AES.MODE_ECB)
     cipher_text = b""
-    header, data = read_file(f)
     data = pad_fun(data)
     for i in range(0, len(data), 16):
         one_block = data[i:i+16]
         cipher_text += cipher.encrypt(one_block)
-    return header, cipher_text
+    return cipher_text
+
+def ecb_decrypt(ciphertext, key):
+    cipher = AES.new(key,AES.MODE_ECB)
+    plaintext = b""
+    for i in range(0, len(ciphertext), 16):
+        one_block = ciphertext[i:i+16]
+        plaintext += cipher.decrypt(one_block)
+    plaintext = unpad_fun(plaintext)
+    return plaintext
     
 def cbc_encrypt(file, key, iv):
     ...
@@ -39,8 +50,11 @@ def cbc_encrypt(file, key, iv):
 def main():
     key = generate_key()
     iv = generate_iv()
-    header, ecb_cipher = ecb_encrypt("mustang.bmp", key)
+    header, data = read_file("mustang.bmp")
+    ecb_cipher = ecb_encrypt(data, key)
     write_file("ecb_mustang_encrypted.bmp", header, ecb_cipher)
+    if ecb_decrypt(ecb_cipher, key) == data:
+        print("Successfully decrypted ecb_mustang")
 
 
 if __name__ == '__main__':
