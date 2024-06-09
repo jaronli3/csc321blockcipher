75d81a0abc3871b76062f6efe07d520d4b2d09e9
jaronli3 <2002jaron@gmail.com>
2024-06-09 16:12:47 -0700

    did bit flip

--- limits_of_confidentiality.py
+++ limits_of_confidentiality.py
@@ -5,43 +5,49 @@ import random
 
 def submit(user_input, key, iv):
     prefix = b"userid=456; userdata="
+    print(len(prefix))
     suffix = b";session-id=31337"
     new_user_input = urllib.parse.quote(user_input)
     new_user_input = new_user_input.encode()
     text = prefix + new_user_input + suffix
+    print(f"Encrypting: {text}")
     cipher_text = MOP.cbc_encrypt(text, key, iv)
-    return cipher_text, len(MOP.pad_fun(text)) // 16
+    return cipher_text
 
 def verify(ciphertext, key, iv):
     plain_text = MOP.cbc_decrypt(ciphertext, key, iv)
-    print(plain_text)
+    print(f"Decrypted: {plain_text}")
     if b";admin=true;" in plain_text:
         return True
     return False
 
-def flip_bit(ciphertext, num_blocks, block_size=16):
+def flip_bit(ciphertext):
     modify_cipher = bytearray(ciphertext)
-    admin_true_text = b";admin=true;"
-    start = random.randint(1, num_blocks - 1) * block_size
-    prev = ciphertext[start - block_size: start]
-    xor = MOP.xor_func(admin_true_text, prev)
-    for i in range(len(admin_true_text)):
-        modify_cipher[start+i] ^= xor[i]
-
+    byte_pos = [33, 39, 44]
+    for x in byte_pos:
+        block = x // 16
+        index = x % 16
+        prev = (block - 1) * 16
+        if index == 1:
+            modify_cipher[prev + index] ^= (ord("X") ^ ord (";"))
+        elif index == 7:
+            modify_cipher[prev + index] ^= (ord("Y") ^ ord ("="))
+        elif index == 12:
+            modify_cipher[prev + index] ^= (ord("X") ^ ord (";"))
     return bytes(modify_cipher)
 
 def main():
     key = MOP.generate_key()
     iv = MOP.generate_iv()
-    user_input = input("Give a string: ")
-    ciphertext, num_blocks = submit(user_input, key, iv)
+    user_input = "AAAAAAAAAAAAXadminYtrueX"
+    ciphertext= submit(user_input, key, iv)
     if verify(ciphertext, key, iv) is True:
         print("verify function returned true")
     else:
         print("verify function returned false")
 
-    flipped = flip_bit(ciphertext, num_blocks)
-
+    flipped = flip_bit(ciphertext)
+    # print(flipped)
     if verify(flipped, key, iv) is True:
         print("verify with flip bit function returned true")
     else:
